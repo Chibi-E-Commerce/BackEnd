b50673a1b813590f6424a35e9d4c5d1bd8e98c0d
Pedro Gabriel <pedro.correia@germinare.org.br>
2025-02-26 10:14:21 -0300

    Criação dos Services e Controllers. Completando CRUD Básico.

---
+++ src/main/java/com/example/Chibi/service/ProductService.java
@@ -0,0 +1,46 @@
+package com.example.Chibi.service;
+
+import com.example.Chibi.dto.ProductDto;
+import com.example.Chibi.model.ProductModel;
+import com.example.Chibi.repository.ProductRepository;
+import lombok.AllArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Service
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductModel insert(ProductDto productDto) {
+        ProductModel productModel = new ProductModel();
+        BeanUtils.copyProperties(productDto, productModel);
+        return productRepository.save(productModel);
+    }
+
+    public ProductModel findById(String id) {
+        return productRepository.findById(new ObjectId(id)).orElse(null);
+    }
+
+    public List<ProductModel> findAll() {
+        return productRepository.findAll();
+    }
+
+    public ProductModel update(String id, ProductDto productDto) {
+        ProductModel productModel = findById(id);
+        if (productModel != null) {
+            BeanUtils.copyProperties(productDto, productModel);
+            return productRepository.save(productModel);
+        }
+        return null;
+    }
+
+    public void delete(String id) {
+        productRepository.deleteById(new ObjectId(id));
+    }
+}
