5905cf33650f54e3c2443dd5bf64ce4cb7c0c6c6
Pedro Gabriel <pedro.correia@germinare.org.br>
2025-02-27 08:34:32 -0300

    Aplicando boas práticas para resposta da API.

--- src/main/java/com/example/Chibi/dto/ProductDto.java
+++ src/main/java/com/example/Chibi/dto/ProductDto.java
@@ -1,5 +1,6 @@
 package com.example.Chibi.dto;
 
+import com.example.Chibi.model.ProductModel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -20,4 +21,14 @@ public class ProductDto {
     private String urlImagem;
     private double desconto;
     private List<String> categoria;
+
+    public ProductDto(ProductModel model){
+        this.nome = model.getNome();
+        this.descricao = model.getDescricao();
+        this.preco = model.getPreco();
+        this.marca = model.getMarca();
+        this.urlImagem = model.getUrlImagem();
+        this.desconto = model.getDesconto();
+        this.categoria = model.getCategoria();
+    }
 }
