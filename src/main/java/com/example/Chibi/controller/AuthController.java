2de183d5ad9a4033a935fc7d7a5a4797962bc0ac
taxiotti <lucas.taxiotti@germinare.org.br>
2025-03-23 18:18:06 -0300

    O extrato baixa tanto em formato txt quanto pdf

--- src/main/java/com/example/Chibi/controller/AuthController.java
+++ src/main/java/com/example/Chibi/controller/AuthController.java
@@ -22,7 +22,7 @@ public class AuthController {
         boolean userValid = clientService.isUserValid(loginRequest.getEmail(), loginRequest.getSenha());
 
         if (!userValid) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("E-mail ou senha incorretos.");
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("E-mail ou senha incorretos!");
         }
 
         return ResponseEntity.ok("Login bem-sucedido!");
