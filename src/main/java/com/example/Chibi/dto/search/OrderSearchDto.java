c30ee109da199c3c4f745dcb2a31fdc985f7fbd5
Pedro Gabriel <pedro.correia@germinare.org.br>
2025-04-13 17:20:22 -0300

    Ajustes Últimos Últimos Finais

--- src/main/java/com/example/Chibi/dto/search/OrderSearchDto.java
+++ src/main/java/com/example/Chibi/dto/search/OrderSearchDto.java
@@ -12,6 +12,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.function.Predicate;
 
